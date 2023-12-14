2e180932169ef0f6a084696827654c33146cb9f0
marie <ingabiremarieaimee6@gmail.com>
2023-12-14 10:43:22 -0800

    my push

---
+++ momo.js
@@ -0,0 +1,69 @@
+let choice =Number(prompt("enter your number:1,2,3,4,5,6,7"));
+switch(choice){
+ case 1:
+    alert = "lohereza amafranga";
+if(choice==1){
+    let choice=Number(prompt("enter your choice:1,2,3"));
+    switch(choice){
+        case 1:
+            alert="uri muri momo";
+         if (choice ==1){
+            let choice =Number(prompt("enter number: 07xxx"));
+            let name = "marcelline";
+            alert=("emeza umubare wa mafranga woherereza",+name);
+            prompt("enter amount to sent");
+            alert="press ok to send";
+
+         }
+         
+         break;
+        case 2:
+            alert="ohereza kuri tigo cg airtel";
+            if (choice ==2){
+               let choice =Number(prompt("enter number: 07xxx"));
+               let name = "marcelline";
+               alert=("emeza umubare wa mafranga woherereza",+name);
+               prompt("enter amount to sent");
+               alert="press ok to send";
+            }
+        break;
+        case 3:
+        alert="ohereza mu mahanga";
+        if (choice ==2){
+            let choice =Number(prompt("enter number: 07xxx"));
+            let name = "marcelline";
+            alert=("emeza umubare wa mafranga woherereza",+name);
+            prompt("enter amount to sent");
+            alert="press ok to send";
+         }
+
+        break;
+         default:
+        alert = "unknown service";
+         break;
+    }
+}
+
+ break;
+case 2:
+ alert= "kugura"; 
+break;
+case 3:
+    alert="kwishyura"
+break;
+case 4:
+ alert="service za bank "
+break;
+case 5:
+    alert="inguzanyo no kuzigama"
+break;
+case 6:
+alert="konti ya momo"
+break;
+case 7:
+    alert="ibyemezo binyuranye"
+default:
+   alert = "unknown service";
+break;
+}
+console.log("choice")
